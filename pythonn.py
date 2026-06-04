91f03d905b1c83a98dff6175ee7c1587d57226c8
NiranjanHulamudde <Niranjan.Hulamudde123@gmail.com>
2026-06-05 00:30:08 +0530

    added other python file

---
+++ pythonn.py
@@ -0,0 +1,16 @@
+le True:
+            print("1. Check SSH")
+                print("2. Check HTTP")
+                    print("3. Exit")
+                        
+                            choice = input("Please select one of the above: ")
+                                
+                                    if choice == "1":
+                                                print("SSH is running on port 22")
+                                                    elif choice == "2":
+                                                                print("HTTP is running on 80")
+                                                                    elif choice == "3":
+                                                                                print("Thank you!")
+                                                                                        break
+                                                                                        else:
+                                                                                                    print("Invalid choice")
